988ca61a862b82fa01e6e54a35c5e161b9236b70
Diger <kitten.diger@gmail.com>
2023-07-29 17:25:30 +0900

    init

---
+++ src/main/kotlin/com/diger/designpatternwithspring/DesignPatternWithSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.diger.designpatternwithspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesignPatternWithSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<DesignPatternWithSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/diger/designpatternwithspring/DesignPatternWithSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diger.designpatternwithspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesignPatternWithSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
